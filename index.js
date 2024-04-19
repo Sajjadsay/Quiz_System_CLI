e6ac4899be9f133a9e71bc08fc60b73316e54af6
Muhammad Sajjad <157211977+Sajjadsay@users.noreply.github.com>
2024-04-19 11:56:31 +0500

    project-08

---
+++ index.js
@@ -0,0 +1,129 @@
+import inquirer from "inquirer";
+import { questions } from "./questions.js";
+let index = 0;
+let score = 0;
+for (let i = 1; i <= 10; i++) {
+    let result = await inquirer.prompt([
+        {
+            type: "list",
+            name: "mychoice",
+            message: questions[index],
+            choices: ["a", "b", "c", "d"]
+        },
+    ]);
+    if (index === 0) {
+        if (result.mychoice === "b") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'b'");
+        }
+    }
+    if (index === 1) {
+        if (result.mychoice === "c") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'c'");
+        }
+    }
+    if (index === 2) {
+        if (result.mychoice === "b") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'b'");
+        }
+    }
+    if (index === 3) {
+        if (result.mychoice === "b") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'b'");
+        }
+    }
+    if (index === 4) {
+        if (result.mychoice === "d") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'd'");
+        }
+    }
+    if (index === 5) {
+        if (result.mychoice === "d") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'd'");
+        }
+    }
+    if (index === 6) {
+        if (result.mychoice === "b") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'b'");
+        }
+    }
+    if (index === 7) {
+        if (result.mychoice === "d") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'd'");
+        }
+    }
+    if (index === 8) {
+        if (result.mychoice === "c") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'c'");
+        }
+    }
+    if (index === 9) {
+        if (result.mychoice === "a") {
+            console.log("Your answer is correct!");
+            score++;
+        }
+        else {
+            console.log("Your answer is wrong!");
+            console.log("The correct answer is 'a'");
+        }
+    }
+    index++;
+}
+console.log("Wait for just tow seconds,your result is ready now...");
+setTimeout(() => {
+    console.clear();
+    if (score > 4) {
+        console.log("Congratulations***You are passed.");
+        console.log("Your result is ", score * 10, "%");
+        console.log("Thanks for bieg here...");
+    }
+    else {
+        console.log("You are failed!.");
+        console.log("Your result is ", score * 10, "%.");
+        console.log("Try it better.Thanks for bieg here...");
+    }
+}, 2000);
---
+++ questions.js
@@ -0,0 +1,12 @@
+export const questions = [
+    "1) What does the given expression evaluate to ? E=6+5*4%3:\n a) 1 \n b) 8\n c) 11\n d) 9",
+    "2) The location number of an item in an array is called:\n a) Data \n b) Value\n c) Index\n d) Constant",
+    "3) The collection of similar data type items in a contiguous memory location is called:\n a) Record \n b) Array\n c) Program\n d) File",
+    "4) Which of the following is not a looping statement:\n a) while \n b) until\n c) do\n d) for",
+    "5) An array of characters is called:\n a) String \n b) Character array\n c) Constant array\n d) both a&b",
+    "6) Which of the folling is a selection statement:\n a) break \n b) goto\n c) exit\n d) switch",
+    "7) A constructor is called(execute) whenever:\n a) An object is destroyed. \n b) An object is created.\n c) A class is declared.\n d) A class is used.",
+    "8) The name of destructor is always preceded by the symbol:\n a) + \n b) - \n c) %\n d) ~",
+    "9) Inheritance is used to:\n a) Increase the size of a program. \n b) Make the program simpler.\n c) Provide the facility of code reusability.\n d) Provide the facility of data hiding.",
+    "10) Funtion overloading is used to achieve:\n a) class \n b) array\n c) object\n d) both a&c",
+];
